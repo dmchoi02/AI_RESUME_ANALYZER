32b57c876694d6da641ea9ae172bc4c6626c4d47
DongMin Choi <dmchoi92@naver.com>
2023-11-15 01:35:55 +0900

    코드 구조 수정 및 기초 세팅 1. import 코드 Imports.py로 이동 2. 기존 App.py에 있던 함수들 Functions.py로 이동 3. App.py 일부분 분석하고 한글로 주석 작성

--- App/App.py
+++ App/App.py
@@ -1,139 +1,25 @@
 # Developed by dnoobnerd [https://dnoobnerd.netlify.app]    Made with Streamlit
-import nltk
-nltk.download('stopwords')
-#nltk.download('punkt')
 
 
+# 모든 페이지에서 import는 이렇게 치면 됩니다
+from Imports import *
 
-
-###### Packages Used ######
-import streamlit as st # core package used in this project
-import pandas as pd
-import base64, random
-import time,datetime
-import pymysql
-import os
-import socket
-import platform
-import geocoder
-import secrets
-import io,random
-import plotly.express as px # to create visualisations at the admin session
-import plotly.graph_objects as go
-from geopy.geocoders import Nominatim
-# libraries used to parse the pdf files
-from pyresparser import ResumeParser
-from pdfminer3.layout import LAParams, LTTextBox
-from pdfminer3.pdfpage import PDFPage
-from pdfminer3.pdfinterp import PDFResourceManager
-from pdfminer3.pdfinterp import PDFPageInterpreter
-from pdfminer3.converter import TextConverter
-from streamlit_tags import st_tags
-from PIL import Image
-# pre stored data for prediction purposes
-from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
-import nltk
-nltk.download('stopwords')
-
-
-###### Preprocessing functions ######
-
-
-# Generates a link allowing the data in a given panda dataframe to be downloaded in csv format 
-def get_csv_download_link(df,filename,text):
-    csv = df.to_csv(index=False)
-    ## bytes conversions
-    b64 = base64.b64encode(csv.encode()).decode()      
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
-    return href
-
-
-# Reads Pdf file and check_extractable
-def pdf_reader(file):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    with open(file, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-            print(page)
-        text = fake_file_handle.getvalue()
-
-    ## close open handles
-    converter.close()
-    fake_file_handle.close()
-    return text
-
-
-# show uploaded file path to view pdf_display
-def show_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-# course recommendations which has data already loaded from Courses.py
-def course_recommender(course_list):
-    st.subheader("**Courses & Certificates Recommendations 👨‍🎓**")
-    c = 0
-    rec_course = []
-    ## slider to choose from range 1-10
-    no_of_reco = st.slider('Choose Number of Course Recommendations:', 1, 10, 5)
-    random.shuffle(course_list)
-    for c_name, c_link in course_list:
-        c += 1
-        st.markdown(f"({c}) [{c_name}]({c_link})")
-        rec_course.append(c_name)
-        if c == no_of_reco:
-            break
-    return rec_course
-
-
-###### Database Stuffs ######
 
 
 # sql connector
 #connection = pymysql.connect(host='localhost',user='root',password='root@MySQL4admin',db='cv')
-connection = pymysql.connect(host='localhost',user='root',password='0000',db='cv')
+connection = pymysql.connect(host='localhost',user='root',password='0000',db='cv') #mysql과 연결
 cursor = connection.cursor()
 
 
-# inserting miscellaneous data, fetched results, prediction and recommendation into user_data table
-def insert_data(sec_token,ip_add,host_name,dev_user,os_name_ver,latlong,city,state,country,act_name,act_mail,act_mob,name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses,pdf_name):
-    DB_table_name = 'user_data'
-    insert_sql = "insert into " + DB_table_name + """
-    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-    rec_values = (str(sec_token),str(ip_add),host_name,dev_user,os_name_ver,str(latlong),city,state,country,act_name,act_mail,act_mob,name,email,str(res_score),timestamp,str(no_of_pages),reco_field,cand_level,skills,recommended_skills,courses,pdf_name)
-    cursor.execute(insert_sql, rec_values)
-    connection.commit()
-
-
-# inserting feedback data into user_feedback table
-def insertf_data(feed_name,feed_email,feed_score,comments,Timestamp):
-    DBf_table_name = 'user_feedback'
-    insertfeed_sql = "insert into " + DBf_table_name + """
-    values (0,%s,%s,%s,%s,%s)"""
-    rec_values = (feed_name, feed_email, feed_score, comments, Timestamp)
-    cursor.execute(insertfeed_sql, rec_values)
-    connection.commit()
-
-
 ###### Setting Page Configuration (favicon, Logo, Title) ######
-
-
 st.set_page_config(
-   page_title="AI Resume Analyzer",
-   page_icon='./Logo/recommend.png',
+   page_title="AI Resume Analyzer", #페이지 제목
+   page_icon='./Logo/recommend.png', #페이지 로고
 )
 
 
-###### Main function run() ######
-
-
+###### Main function run() ###### 
 def run():
     
     # (Logo, Heading, Sidebar etc)
@@ -142,8 +28,8 @@ def run():
     #img = Image.open('./APP/LOGO/RESUM.png')
 
     st.image(img)
-    st.sidebar.markdown("# Choose Something...")
-    activities = ["User", "Feedback", "About", "Admin"]
+    st.sidebar.markdown("# Choose Something...") 
+    activities = ["User", "Feedback", "About", "Admin"] # 목록
     choice = st.sidebar.selectbox("Choose among the given options:", activities)
     link = '<b>Built with 🤍 by <a href="https://dnoobnerd.netlify.app/" style="text-decoration: none; color: #021659;">Deepak Padhi</a></b>' 
     st.sidebar.markdown(link, unsafe_allow_html=True)
@@ -163,12 +49,9 @@ def run():
     ''', unsafe_allow_html=True)
 
     ###### Creating Database and Table ######
-
-
     # Create the DB
     db_sql = """CREATE DATABASE IF NOT EXISTS CV;"""
     cursor.execute(db_sql)
-
 
     # Create table user_data and user_feedback
     DB_table_name = 'user_data'
@@ -202,7 +85,6 @@ def run():
                 """
     cursor.execute(table_sql)
 
-
     DBf_table_name = 'user_feedback'
     tablef_sql = "CREATE TABLE IF NOT EXISTS " + DBf_table_name + """
                     (ID INT NOT NULL AUTO_INCREMENT,
@@ -217,9 +99,17 @@ def run():
     cursor.execute(tablef_sql)
 
 
+
+
+
+
+
+
+
+
     ###### CODE FOR CLIENT SIDE (USER) ######
 
-    if choice == 'User':
+    if choice == 'User': # 목록에서 유저 선택한 경우
         
         # Collecting Miscellaneous Information
         act_name = st.text_input('Name*')
@@ -233,7 +123,7 @@ def run():
         g = geocoder.ip('me')
         latlong = g.latlng
         geolocator = Nominatim(user_agent="http")
-        location = geolocator.reverse(latlong, language='en')
+        location = geolocator.reverse(latlong, language='kr')
         address = location.raw['address']
         cityy = address.get('city', '')
         statee = address.get('state', '')
@@ -565,16 +455,18 @@ def run():
 
 
                 ## Calling insert_data to add all the data into user_data                
-                insert_data(str(sec_token), str(ip_add), (host_name), (dev_user), (os_name_ver), (latlong), (city), (state), (country), (act_name), (act_mail), (act_mob), resume_data['name'], resume_data['email'], str(resume_score), timestamp, str(resume_data['no_of_pages']), reco_field, cand_level, str(resume_data['skills']), str(recommended_skills), str(rec_course), pdf_name)
+                insert_data(cursor, connection, str(sec_token), str(ip_add), (host_name), (dev_user), (os_name_ver), (latlong), (city), (state), (country), (act_name), (act_mail), (act_mob), resume_data['name'], resume_data['email'], str(resume_score), timestamp, str(resume_data['no_of_pages']), reco_field, cand_level, str(resume_data['skills']), str(recommended_skills), str(rec_course), pdf_name)
 
                 ## Recommending Resume Writing Video
-                st.header("**Bonus Video for Resume Writing Tips💡**")
-                resume_vid = random.choice(resume_videos)
+                #st.header("**Bonus Video for Resume Writing Tips💡**")
+                st.header("**이력서 작성을 위한 보너스 영상💡**")
+                resume_vid = random.choice(resume_videos) #랜덤으로 선택
                 st.video(resume_vid)
 
                 ## Recommending Interview Preparation Video
-                st.header("**Bonus Video for Interview Tips💡**")
-                interview_vid = random.choice(interview_videos)
+                #st.header("**Bonus Video for Interview Tips💡**")
+                st.header("**면접을 위한 보너스 영상💡**")
+                interview_vid = random.choice(interview_videos) #랜덤으로 선택
                 st.video(interview_vid)
 
                 ## On Successful Result 
@@ -604,7 +496,7 @@ def run():
             submitted = st.form_submit_button("Submit")
             if submitted:
                 ## Calling insertf_data to add dat into user feedback
-                insertf_data(feed_name,feed_email,feed_score,comments,Timestamp)    
+                insertf_data(cursor, connection, feed_name,feed_email,feed_score,comments,Timestamp)    
                 ## Success Message 
                 st.success("Thanks! Your Feedback was recorded.") 
                 ## On Successful Submit
