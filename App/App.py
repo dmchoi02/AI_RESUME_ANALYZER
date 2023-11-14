4346a4758dd57fd7f7e66f1207c741068ffe5227
DongMin Choi <dmchoi92@naver.com>
2023-11-15 01:42:37 +0900

    App.py 잘못 올려서 다시 수정

--- App/App.py
+++ App/App.py
@@ -5,98 +5,9 @@ nltk.download('stopwords')
 
 
 
+from Imports import *
 
 
-
-
-###### Packages Used ######
-import streamlit as st # core package used in this project
-import pandas as pd
-import base64, random
-import time,datetime
-import pymysql
-import os
-import socket
-import platform
-import geocoder
-import secrets
-import io,random
-import plotly.express as px # to create visualisations at the admin session
-import plotly.graph_objects as go
-from geopy.geocoders import Nominatim
-# libraries used to parse the pdf files
-from pyresparser import ResumeParser
-from pdfminer3.layout import LAParams, LTTextBox
-from pdfminer3.pdfpage import PDFPage
-from pdfminer3.pdfinterp import PDFResourceManager
-from pdfminer3.pdfinterp import PDFPageInterpreter
-from pdfminer3.converter import TextConverter
-from streamlit_tags import st_tags
-from PIL import Image
-# pre stored data for prediction purposes
-from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
-import nltk
-nltk.download('stopwords')
-
-
-###### Preprocessing functions ######
-
-
-# Generates a link allowing the data in a given panda dataframe to be downloaded in csv format 
-def get_csv_download_link(df,filename,text):
-    csv = df.to_csv(index=False)
-    ## bytes conversions
-    b64 = base64.b64encode(csv.encode()).decode()      
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
-    return href
-
-
-# Reads Pdf file and check_extractable
-def pdf_reader(file):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    with open(file, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-            print(page)
-        text = fake_file_handle.getvalue()
-
-    ## close open handles
-    converter.close()
-    fake_file_handle.close()
-    return text
-
-
-# show uploaded file path to view pdf_display
-def show_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-# course recommendations which has data already loaded from Courses.py
-def course_recommender(course_list):
-    st.subheader("**Courses & Certificates Recommendations 👨‍🎓**")
-    c = 0
-    rec_course = []
-    ## slider to choose from range 1-10
-    no_of_reco = st.slider('Choose Number of Course Recommendations:', 1, 10, 5)
-    random.shuffle(course_list)
-    for c_name, c_link in course_list:
-        c += 1
-        st.markdown(f"({c}) [{c_name}]({c_link})")
-        rec_course.append(c_name)
-        if c == no_of_reco:
-            break
-    return rec_course
-
-
-###### Database Stuffs ######
 
 
 # sql connector
